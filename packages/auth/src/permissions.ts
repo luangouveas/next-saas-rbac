c026f3b9cf40ba4c6ad0efef20edf4a7c4e013a2
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-08 00:17:32 -0300

    Implementando pacote auth com validação de permissões

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,20 @@
+import { AbilityBuilder } from '@casl/ability'
+
+import { AppAbility } from '.'
+import { User } from './models/user'
+
+type Role = 'ADMIN' | 'MEMBER'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>,
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN(_user, { can }) {
+    can('manage', 'all')
+  },
+  MEMBER(_user, { can }) {
+    can('invite', 'User')
+  },
+}
