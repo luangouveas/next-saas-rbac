1d02be8599570022ddf72c6c382f8e7c45cc1154
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-10 00:04:29 -0300

    Rota para cadastro de novo usuário

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,48 @@
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithEmail) {
+        reply
+          .status(400)
+          .send({ message: 'Já existe um usuário com este email cadastrado.' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      reply.status(201).send()
+    },
+  )
+}
