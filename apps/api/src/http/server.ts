fe1c9d57d35759977ae85b3db9fd211d5e942320
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-10 00:42:26 -0300

    Configurar rota para documentação interativa com swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,17 +1,35 @@
 import fastifyCors from '@fastify/cors'
-import { fastify } from 'fastify'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
-import { createAccount } from './routes/auth/create-account'
+import { createAccount } from '@/http/routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS app with multi-tenant & RABC.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
