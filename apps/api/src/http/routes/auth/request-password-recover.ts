925397edff6779668c6c7acce3d39981a8ef9bd1
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-11 15:04:10 -0300

    Cria rota para solicitação de recuperação de senha do usuário

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFormEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFormEmail) {
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSORD_RECOVER',
+          userId: userFormEmail.id,
+        },
+      })
+
+      // Send e-mail with password recover link
+
+      console.log('Recover password code', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
