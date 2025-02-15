126c5110d5caee90fe9c26223a6022b67568b70a
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-14 23:23:11 -0300

    Cria rota que consulta dados de um projeto

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -28,6 +28,7 @@ import { transferOrganization } from './routes/orgs/tarnsfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -83,6 +84,7 @@ app.register(transferOrganization)
 
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP Server running on http://localhost:${env.SERVER_PORT}`)
