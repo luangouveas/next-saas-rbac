a1b4bcbc856868e514c059b7ac0babb93ad789a0
Luan Gouveas <lgouveas@rhmed.com.br>
2025-02-14 23:18:32 -0300

    Cria rota que deleta um projeto

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/tarnsfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
+import { deleteProject } from './routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -81,6 +82,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP Server running on http://localhost:${env.SERVER_PORT}`)
