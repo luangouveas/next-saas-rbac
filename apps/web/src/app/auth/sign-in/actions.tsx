478d9d05f783418f3adb99869629641bf228f2d3
Luan Gouveas <lgouveas@rhmed.com.br>
2025-04-02 14:21:41 -0300

    Finaliza estratégia de autenticação via email e senha

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,14 +1,63 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
+const SignInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Please, provide your password.' }),
+})
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+  const result = SignInSchema.safeParse(Object.fromEntries(data))
 
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
 
-  console.log(result)
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+
+    const cookieStore = await cookies()
+
+    cookieStore.set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.log(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  redirect('/')
 }
