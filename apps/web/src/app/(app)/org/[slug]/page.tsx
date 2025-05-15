4afd1df4746357a330ad5fbec449c65da6cdacef
Luan Gouveas <lgouveas@rhmed.com.br>
2025-05-15 00:41:29 -0300

    Lista projetos

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function ProjectsPage() {
-  return (
-    <div className="space-y-4 py-4">
-      <main className="mx-auto w-full max-w-[1200px]">Projects</main>
-    </div>
-  )
-}
