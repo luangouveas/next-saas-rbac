e3df8c80a4b0f93122e70b03aa7c1738490622d7
Luan Gouveas <lgouveas@rhmed.com.br>
2025-05-15 01:23:48 -0300

    Pagina de ação em convites recebidos

--- apps/web/src/app/auth/sign-in/actions.ts
+++ apps/web/src/app/auth/sign-in/actions.ts
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
+import { acceptInvite } from '@/http/accept-invite'
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
 const SignInSchema = z.object({
@@ -40,6 +41,15 @@ export async function signInWithEmailAndPassword(data: FormData) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     })
+
+    const inviteId = cookieStore.get('inviteId')?.value
+
+    if (inviteId) {
+      try {
+        await acceptInvite(inviteId)
+        cookieStore.delete('inviteId')
+      } catch {}
+    }
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
