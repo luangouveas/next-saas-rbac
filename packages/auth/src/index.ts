2ad80dd6619f84dee7a71ac5b04730b46e20dfb7
Luan Gouveas <lgouveas@rhmed.com.br>
2025-05-19 11:26:50 -0300

    Reorganiza imports

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -14,12 +14,17 @@ import { organizationSubject } from './subjects/organization'
 import { projectSubject } from './subjects/project'
 import { userSubject } from './subjects/user'
 
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
+export * from './roles'
+
 const appAbilitiesSchema = z.union([
-  billingSubject,
-  inviteSubject,
-  organizationSubject,
   projectSubject,
   userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
   z.tuple([z.literal('manage'), z.literal('all')]),
 ])
 
@@ -27,11 +32,6 @@ type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
-
-export * from './models/organization'
-export * from './models/project'
-export * from './models/user'
-export * from './roles'
 
 export function defineAbilityFor(user: User) {
   const builder = new AbilityBuilder(createAppAbility)
